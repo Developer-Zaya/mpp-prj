00ec3439688a69970e0415bb63314611c6ccdbd3
jimmy <saraasoulmate@gmail.com>
2022-12-09 19:56:15 -0600

    checkout diagram

--- src/windowindex/LibrarianConstants.java
+++ src/windowindex/LibrarianConstants.java
@@ -4,4 +4,6 @@ public class LibrarianConstants {
 	public final static String DETAIL ="Logout";
 	public final static String CHECKOUT_MEMBER = "Checkout Member";
 	public final static  String CHECKOUT_BOOK = "Checkout Book";
+
+	public final static  String OVER_DUE_BOOK = "Over due Books";
 }
